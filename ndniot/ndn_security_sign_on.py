72e871ce806c9d5b995841ff2f1c0f8668533fe8
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2019-12-09 20:19:37 -0800

    migrate to python-ndn, not finished

---
+++ ndniot/ndn_security_sign_on.py
@@ -0,0 +1,31 @@
+from ndn.encoding import TlvModel, BytesField, RepeatedField, ModelField, UintField, TypeNumber, NameField
+from ndn.app_support.security_v2 import CertificateV2Value
+
+TLV_SEC_BOOT_ANCHOR_DIGEST = 161
+TLV_SEC_BOOT_CAPABILITIES = 160
+TLV_AC_ECDH_PUB_N1 = 162
+TLV_AC_ECDH_PUB_N2 = 163
+TLV_AC_SALT = 131
+TLV_AC_AES_IV = 135
+TLV_AC_ENCRYPTED_PAYLOAD = 136
+
+class SignOnRequest(TlvModel):
+    identifier = BytesField(TypeNumber.GENERIC_NAME_COMPONENT)
+    capabilities = BytesField(TLV_SEC_BOOT_CAPABILITIES)
+    ecdh_n1 = BytesField(TLV_AC_ECDH_PUB_N1)
+
+class SignOnResponse(TlvModel):
+    salt = BytesField(TLV_AC_SALT)
+    ecdh_n2 = BytesField(TLV_AC_ECDH_PUB_N2)
+    anchor = ModelField(TypeNumber.DATA, CertificateV2Value)
+
+class CertRequest(TlvModel):
+    identifier = BytesField(TypeNumber.GENERIC_NAME_COMPONENT)
+    ecdh_n2 = BytesField(TLV_AC_ECDH_PUB_N2)
+    anchor_digest = BytesField(TLV_SEC_BOOT_ANCHOR_DIGEST)
+    ecdh_n1 = BytesField(TLV_AC_ECDH_PUB_N1)
+
+class CertResponse(TlvModel):
+    cipher = BytesField(TLV_AC_ENCRYPTED_PAYLOAD)
+    iv = BytesField(TLV_AC_AES_IV)
+    id_cert = ModelField(TypeNumber.DATA, CertificateV2Value)
